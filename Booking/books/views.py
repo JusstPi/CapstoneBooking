49d6d34c9d81d6c37f40dbd91382bfd68032a8e2
JustPi <pamela.canedo@cit.edu>
2023-05-24 16:35:09 +0800

    views init

--- Booking/books/views.py
+++ Booking/books/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import CalendarEvent,User, Venue, Booking, BookingAttendee
+from .serializers import UserSerializer, VenueSerializer, BookingSerializer
 
-# Create your views here.
+
+class Useriew(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class VenueView(generics.ListCreateAPIView):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+class BookingView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class BookingDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
